b785a4d778c486b0d06d67b7bb87745ae0885f60
Imre Kószó <imrekoszo@users.noreply.github.com>
2020-12-01 13:19:06 +0100

    Reformat to https://tonsky.me/blog/clojurefmt/

--- src/imrekoszo/advent2020/day1.clj
+++ src/imrekoszo/advent2020/day1.clj
@@ -3,17 +3,16 @@
 
 (defonce input1
   (->> "day1-input1.txt"
-       io/resource
-       io/reader
-       line-seq
-       (map #(Integer/parseInt %))))
+    io/resource
+    io/reader
+    line-seq
+    (map #(Integer/parseInt %))))
 
 (def first-match
-  (partial
-   reduce
-   #(when (= 2020 (apply + %2))
-      (reduced (apply * %2)))
-   nil))
+  (partial reduce
+    #(when (= 2020 (apply + %2))
+       (reduced (apply * %2)))
+    nil))
 
 (defn result1 []
   (first-match (for [a input1 b input1] [a b])))
