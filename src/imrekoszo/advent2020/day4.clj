456118c400a2639043e45598759d9d36061c89d1
Imre Kószó <imrekoszo@users.noreply.github.com>
2020-12-05 22:34:22 +0100

    Solve day 4 part 2

--- src/imrekoszo/advent2020/day4.clj
+++ src/imrekoszo/advent2020/day4.clj
@@ -26,27 +26,76 @@
 
 (def load-input! (u/parse-input-fn parse-input))
 (def test-input* (delay (load-input! "4/test.txt")))
+(def test2-input* (delay (load-input! "4/test2.txt")))
 (def live-input* (delay (load-input! "4/input.txt")))
 
-(s/def ::byr any?)
-(s/def ::iyr any?)
-(s/def ::eyr any?)
-(s/def ::hgt any?)
-(s/def ::hcl any?)
-(s/def ::ecl any?)
-(s/def ::pid any?)
-(s/def ::cid any?)
+(s/def :day4.part1/byr any?)
+(s/def :day4.part1/iyr any?)
+(s/def :day4.part1/eyr any?)
+(s/def :day4.part1/hgt any?)
+(s/def :day4.part1/hcl any?)
+(s/def :day4.part1/ecl any?)
+(s/def :day4.part1/pid any?)
+(s/def :day4.part1/cid any?)
 
-(s/def ::passport
-  (s/keys :req-un [::byr ::iyr ::eyr ::hgt ::hcl ::ecl ::pid] :opt-un [::cid]))
+(s/def :day4.part1/passport
+  (s/keys
+    :req-un [:day4.part1/byr :day4.part1/iyr :day4.part1/eyr
+             :day4.part1/hgt :day4.part1/hcl :day4.part1/ecl :day4.part1/pid]
+    :opt-un [:day4.part1/cid]))
+
+(defn calculate [input spec]
+  (x/count (filter #(s/valid? spec %)) input))
 
 (defn part1
   {:test
    #(assert (= 2 (part1 @test-input*)))}
   ([] (part1 @live-input*))
   ([input]
-   (x/count (filter #(s/valid? ::passport %)) input)))
+   (calculate input :day4.part1/passport)))
+
+(defn string-matching? [re]
+  (s/and string? #(re-matches re %)))
+
+(defn int-between [at-least at-most]
+  #(<= at-least % at-most))
+
+(defn int-str-between
+  ([re at-least at-most]
+   (int-str-between re identity at-least at-most))
+  ([re pre-parse at-least at-most]
+   (s/and
+     (string-matching? re)
+     #(-> % pre-parse u/parse-long ((int-between at-least at-most))))))
+
+(defn year-between [at-least at-most]
+  (int-str-between #"\d{4}" at-least at-most))
+
+(s/def :day4.part2/byr (year-between 1920 2002))
+(s/def :day4.part2/iyr (year-between 2010 2020))
+(s/def :day4.part2/eyr (year-between 2020 2030))
+(s/def :day4.part2/hgt (s/or
+                         :cm (int-str-between #"1\d\dcm" #(subs % 0 3) 150 193)
+                         :in (int-str-between #"\d\din" #(subs % 0 2) 59 76)))
+(s/def :day4.part2/hcl (string-matching? #"#[0-9a-f]{6}"))
+(s/def :day4.part2/ecl #{"amb" "blu" "brn" "gry" "grn" "hzl" "oth"})
+(s/def :day4.part2/pid (string-matching? #"\d{9}"))
+(s/def :day4.part2/cid :day4.part1/cid)
+
+(s/def :day4.part2/passport
+  (s/keys
+    :req-un [:day4.part2/byr :day4.part2/iyr :day4.part2/eyr
+             :day4.part2/hgt :day4.part2/hcl :day4.part2/ecl :day4.part2/pid]
+    :opt-un [:day4.part2/cid]))
+
+(defn part2
+  {:test
+   #(assert (= 4 (part2 @test2-input*)))}
+  ([] (part2 @live-input*))
+  ([input]
+   (calculate input :day4.part2/passport)))
 
 (comment
   (part1) ;;=> 260
+  (part2) ;;=> 153
   )
