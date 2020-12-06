bfb8d70ef339da4926b4a82dd79da7448daf0608
Imre Kószó <imrekoszo@users.noreply.github.com>
2020-12-06 21:39:18 +0100

    Solve day 6 part 2

--- src/imrekoszo/advent2020/day6.clj
+++ src/imrekoszo/advent2020/day6.clj
@@ -1,5 +1,6 @@
 (ns imrekoszo.advent2020.day6
-  (:require [imrekoszo.advent2020.util :as u]
+  (:require [clojure.set :as set]
+            [imrekoszo.advent2020.util :as u]
             [net.cgrand.xforms :as x]))
 
 (defn parse-input [lines]
@@ -9,16 +10,39 @@
 (def test-input* (delay (load-input! "6/test.txt")))
 (def live-input* (delay (load-input! "6/input.txt")))
 
-(def count-group-xf
-  (map #(x/count (comp cat (distinct)) %)))
+(defn calculate [grouped-personal-positive-answers count-per-group]
+  (transduce
+    (map count-per-group) +
+    grouped-personal-positive-answers))
 
 (defn part1
   {:test #(assert (= 11 (part1 @test-input*)))}
   ([] (part1 @live-input*))
-  ([groups]
-   (transduce count-group-xf + groups)))
+  ([grouped-personal-positive-answers]
+   (calculate
+     grouped-personal-positive-answers
+     #(x/count (comp cat (distinct)) %))))
+
+(defn part2
+  {:test #(assert (= 6 (part2 @test-input*)))}
+  ([] (part2 @live-input*))
+  ([grouped-personal-positive-answers]
+   (calculate
+     grouped-personal-positive-answers
+     #(count
+        (transduce
+          (comp (drop 1) (map set))
+          (completing
+            (fn [questions-everyone-answered-yes-to-so-far
+                 questions-answered-yes]
+              (let [result (set/intersection
+                             questions-everyone-answered-yes-to-so-far
+                             questions-answered-yes)]
+                (cond-> result (empty? result) (reduced)))))
+          (set (first %))
+          %)))))
 
 (comment
   (part1) ;;=> 6351
+  (part2) ;;=> 3143
   )
-
